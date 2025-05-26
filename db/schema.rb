c13676845cc0678a5b7dff17ec95e97cbdad19f9
William Sebastian <okeefesebastian91@gmail.com>
2025-05-26 17:35:10 +0900

    generate booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_26_083247) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_26_083351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_26_083247) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "band_id", null: false
+    t.bigint "gig_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["band_id"], name: "index_bookings_on_band_id"
+    t.index ["gig_id"], name: "index_bookings_on_gig_id"
+  end
+
   create_table "gigs", force: :cascade do |t|
     t.date "date"
     t.bigint "venue_id", null: false
@@ -112,6 +121,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_26_083247) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "attendances", "gigs"
   add_foreign_key "attendances", "users"
+  add_foreign_key "bookings", "bands"
+  add_foreign_key "bookings", "gigs"
   add_foreign_key "gigs", "users"
   add_foreign_key "gigs", "venues"
   add_foreign_key "users", "bands"
