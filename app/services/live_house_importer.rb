c481eeb988950a37bd6e172cdee71e87a9889a13
Ryan Ward <4577045+Ward-R@users.noreply.github.com>
2025-05-27 17:43:46 +0900

    fixed bugs in parser

--- app/services/live_house_importer.rb
+++ app/services/live_house_importer.rb
@@ -41,13 +41,15 @@ class LiveHouseImporter
       venue_name = gig_attributes['live_house'].to_s.strip
       venue = Venue.find_or_create_by!(name: venue_name) do |v|
         v.neighborhood = gig_attributes['area']
-        v.address = "#{gig_attributes['area'] || 'Unknown Area'}, Tokyo"
+        v.address = "#{gig_attributes['area'] || 'Unknown Area'}, Tokyo",
+        v.email = "#{venue_name}@gmail.com",
+        v.details = "fill in later"
       end
       puts "  Venue: '#{venue.name}' (ID: #{venue.id}) #{venue.new_record? ? 'CREATED' : 'UPDATED'}."
       # Make gig
       adv_price = gig_attributes['adv_price'].to_s.strip
       door_price = gig_attributes['door_price'].to_s.strip
-      price_string = ""
+      price_string = "See at venue"
       if adv_price.present? && door_price.present?
         price_string = "#{adv_price} / #{door_price}"
       elsif adv_price.present?
@@ -57,33 +59,33 @@ class LiveHouseImporter
       end
 
       gig_date = Date.parse(gig_attributes['date'])
-      gig_open_time = gig_attrs['open_time'].to_s.strip
+      gig_open_time = gig_attributes['open_time'].to_s.strip
 
-      gig_attributes = {
+      gig_data = {
           date: gig_date,
           open_time: gig_open_time,
+          start_time: "30 after open_time",
           price: price_string,
           venue: venue,
+          user: User.first
         }
       gig = Gig.find_or_initialize_by(
-          venue: gig_attributes[:venue],
-          date: gig_attributes[:date],
-          open_time: gig_attributes[:open_time]
+          gig_data
         )
       if gig.save
           status = gig.new_record? ? 'CREATED' : 'UPDATED'
           puts "  #{status} Gig (ID: #{gig.id}): '#{full_gig_name}' at #{gig.venue.name} on #{gig.date.strftime('%Y-%m-%d')} #{gig.open_time}"
       end
+
+      if gig.save
+        puts "  Imported/Updated: #{gig.venue.name}"
+      else
+        puts "  ERROR importing #{gig_attributes['name']}: #{gig.errors.full_messages.join(', ')}"
+      end
       # Make bookings
       associated_bands.each do |band|
         Booking.find_or_create_by!(band: band, gig: gig)
         puts "Booking created/ensured for '#{band.name}' on Gig ID #{gig.id}."
-      end
-
-      if gig.save
-        puts "  Imported/Updated: #{gig.name}"
-      else
-        puts "  ERROR importing #{gig_attributes['name']}: #{gig.errors.full_messages.join(', ')}"
       end
     rescue => e
       puts "ERROR processing gig: #{gig_attributes.inspect} - #{e.message}"
