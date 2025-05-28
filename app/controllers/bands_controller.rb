1cddf409a78bca029d57d8d2f4a9e975e4aa432d
William Sebastian <okeefesebastian91@gmail.com>
2025-05-28 17:51:54 +0900

    make gig show page

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,3 +1,12 @@
 class BandsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @bands = Band.all
+  end
+
+  def show
+    @band = Band.find(params[:id])
+    authorize @band
+  end
 end
