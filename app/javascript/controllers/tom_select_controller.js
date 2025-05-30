e2d9008784c5ffa354accd09046c52ecca4ee6e7
William Sebastian <okeefesebastian91@gmail.com>
2025-05-30 12:11:52 +0900

    implement tom select in the genre input

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -1,34 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 import TomSelect from "tom-select"
 
-// Connects to data-controller="tom-select"
 export default class extends Controller {
+  static values = { options: Object }
+
   connect() {
-    new TomSelect(this.element, {
-      plugins: ['remove_button'],
-      create: true,
-      maxItems: 3,
-      render: {
-        option: function(data, escape) {
-          return `<div><i class="fa-solid fa-music me-2"></i> ${escape(data.text)}</div>`;
-        },
-        item: function(data, escape) {
-          return `<div><i class="fa-solid fa-music me-1"></i> ${escape(data.text)}</div>`;
-        }
-      },
-      valueField: "value",
-      labelField: "text",
-      searchField: "text",
-      load: function(query, callback) {
-        if (!query.length) return callback();
-        fetch(`/genres?q=${encodeURIComponent(query)}`)
-          .then(response => response.json())
-          .then(json => {
-            callback(json);
-          }).catch(() => {
-            callback();
-          });
-      },
-      placeholder: "Select or add genres",
-    })
-  };}
+    this.tomSelect = new TomSelect(
+      this.element,
+      this.optionsValue
+    );
+
+    this.tomSelect.onItemAdd = (value, $item) => { this.clearTextbox(); }
+  }
+
+  clearInput(e) {
+    console.log(e, "trigger")
+  }
+}
