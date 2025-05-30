07dc15bb4e97bda06f5b3898c26e02240d952e50
Ryan Ward <4577045+Ward-R@users.noreply.github.com>
2025-05-30 11:39:41 +0900

    added gem favoritor, moved gig route up so it works

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -11,4 +11,6 @@ class Gig < ApplicationRecord
   validates :open_time, presence: true
   validates :start_time, presence: true
   validates :price, presence: true
+
+  acts_as_favoritable # Means that this model can be favourited
 end
