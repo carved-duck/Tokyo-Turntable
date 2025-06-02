35e22abec145ce51a51db75c3fffef9f5b7c16cb
Will Sebastian <167591674+MaddRussian@users.noreply.github.com>
2025-06-02 15:55:04 +0900

    Revert "User list"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
+  def profile
+    @user = current_user
     # Fetch favorited gigs for the current user
     @favorited_gigs = @user.favorited_by_type('Gig')
     # Or, if you want all favorited items regardless of type:
@@ -8,8 +8,14 @@ class UsersController < ApplicationController
     authorize @user # If you are using Pundit
   end
 
-  def index
-    policy_scope(User)
-    @users = User.all
-  end
+  # use this as example for profile then delete after.
+  #   def dashboard
+  #   @rentals = current_user.rentals
+  #   @tools = current_user.tools
+  #   @rentals_as_owner = current_user.rentals_as_owner
+  #   @pending_approvals = @rentals_as_owner.where(status: "pending")
+  #   @waiting_for_return = @rentals_as_owner.where(status: "accepted")
+  #   @my_pending = @rentals.where(status: "pending")
+  #   @my_accepted = @rentals.where(status: "accepted")
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,10 @@ Rails.application.routes.draw do
     resources :bookings, only: [ :index ]
   end
 
-  resources :users, only: [ :index, :show ] do
+  resources :users, only: [ :show ] do
+    collection do
+      get :profile, as: :profile
+    end
     resources :attendances, only: [ :index ]
   end
 
