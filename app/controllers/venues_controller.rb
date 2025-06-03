0a6c2b0e93fcc421a883903fdefd456d70ed10f3
Julian Schoenfeld <juliankick@gmail.com>
2025-06-03 12:03:14 +0900

    Changed the look of the map and venue/gig cards. Started working on a search

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -3,11 +3,22 @@ class VenuesController < ApplicationController
 
   def index
     @venues = policy_scope(Venue)
+    @venues = Venue.all
+    if params[:query].present?
+      # @venues = @venues.joins(gigs: { bookings: :band }).distinct
+      sql_subquery = <<~SQL
+      venues.name ILIKE :query
+      OR venues.neighborhood ILIKE :query
+      OR venues.address ILIKE :query
+      OR bands.name ILIKE :query
+      OR bands.genre ILIKE :query
+      SQL
+      @venues = @venues.joins(gigs: { bookings: :band }).distinct.where(sql_subquery, query: "%#{params[:query]}%")
+    end
   end
 
   def show
     @venue = Venue.find(params[:id])
     authorize @venue
-
   end
 end
